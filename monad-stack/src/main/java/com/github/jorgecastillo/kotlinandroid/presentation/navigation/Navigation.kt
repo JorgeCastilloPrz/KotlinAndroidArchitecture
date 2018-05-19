efbeb3dfea8b5e0c6bb5c03c1a01edcc21321ba4
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-05-19 10:12:25 +0200

    port monad stack to arrow 0.7.1

--- monad-stack/src/main/java/com/github/jorgecastillo/kotlinandroid/presentation/navigation/Navigation.kt
+++ monad-stack/src/main/java/com/github/jorgecastillo/kotlinandroid/presentation/navigation/Navigation.kt
@@ -1,13 +1,12 @@
 package com.github.jorgecastillo.kotlinandroid.presentation.navigation
 
-import arrow.core.ForId
-import arrow.data.Reader
+import arrow.data.ReaderApi
 import arrow.data.map
 import com.github.jorgecastillo.kotlinandroid.di.context.SuperHeroesContext.GetHeroesContext
 import com.github.jorgecastillo.kotlinandroid.view.SuperHeroDetailActivity
 
 class HeroDetailsPage {
-  fun go(heroId: String) = Reader.ask<ForId, GetHeroesContext>().map({ (ctx) ->
+  fun go(heroId: String) = ReaderApi.ask<GetHeroesContext>().map({ (ctx) ->
     SuperHeroDetailActivity.launch(ctx, heroId)
   })
 }
