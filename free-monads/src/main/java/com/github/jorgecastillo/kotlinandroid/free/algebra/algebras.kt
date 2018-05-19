fc46056844b5ec4491a48101ccd67ad35eb4737f
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-05-19 11:51:56 +0200

    half way done porting free module to arrow 0.7.1

--- free-monads/src/main/java/com/github/jorgecastillo/kotlinandroid/free/algebra/algebras.kt
+++ free-monads/src/main/java/com/github/jorgecastillo/kotlinandroid/free/algebra/algebras.kt
@@ -8,40 +8,45 @@ import arrow.free.foldMap
 import arrow.free.instances.FreeMonadInstance
 import arrow.higherkind
 import arrow.typeclasses.Monad
-import arrow.typeclasses.monad
 import com.github.jorgecastillo.kotlinandroid.domain.model.CharacterError
 import com.karumi.marvelapiclient.model.CharacterDto
+
+fun <A> Kind<HeroesAlgebra.F, A>.fix(): HeroesAlgebra<A> = this as HeroesAlgebra<A>
+typealias FreeHeroesAlgebra<A> = Free<HeroesAlgebra.F, A>
+
 
 /**
  * Algebra for Hero data sources. Algebras are defined by a sealed class (ADT) with a limited amount of implementations reflecting the operations available.
  */
 @higherkind
-sealed class HeroesAlgebra<A> : HeroesAlgebraOf<A> {
+sealed class HeroesAlgebra<A> : Kind<HeroesAlgebra.F, A> {
+  class F private constructor()
+
   object GetAll : HeroesAlgebra<List<CharacterDto>>()
   class GetSingle(val heroId: String) : HeroesAlgebra<CharacterDto>()
   class HandlePresentationEffects(val result: Either<CharacterError, List<CharacterDto>>) : HeroesAlgebra<Unit>()
   class Attempt<A>(val fa: FreeHeroesAlgebra<A>) : HeroesAlgebra<Either<Throwable, A>>()
-  companion object : FreeMonadInstance<ForHeroesAlgebra>
+
+  /**
+   * We must lift to the Free context all the operation blocks defined on the algebra and compose
+   * our program logic based on those.
+   */
+  companion object : FreeMonadInstance<F> {
+    fun getAllHeroes(): FreeHeroesAlgebra<List<CharacterDto>> =
+        Free.liftF(HeroesAlgebra.GetAll)
+
+    fun getSingleHero(heroId: String): FreeHeroesAlgebra<CharacterDto> =
+        Free.liftF(HeroesAlgebra.GetSingle(heroId))
+
+    fun handlePresentationEffects(result: Either<CharacterError, List<CharacterDto>>): FreeHeroesAlgebra<Unit> =
+        Free.liftF(HeroesAlgebra.HandlePresentationEffects(result))
+
+    fun <A> attempt(fa: FreeHeroesAlgebra<A>): FreeHeroesAlgebra<Either<Throwable, A>> =
+        Free.liftF(HeroesAlgebra.Attempt(fa))
+
+  }
 }
 
-typealias FreeHeroesAlgebra<A> = Free<ForHeroesAlgebra, A>
-
-inline fun <reified F> Free<ForHeroesAlgebra, List<CharacterDto>>.run(
-    interpreter: FunctionK<ForHeroesAlgebra, F>, MF: Monad<F> = monad()): Kind<F, List<CharacterDto>> =
+inline fun <reified F> Free<HeroesAlgebra.F, List<CharacterDto>>.run(
+    interpreter: FunctionK<HeroesAlgebra.F, F>, MF: Monad<F> = monad()): Kind<F, List<CharacterDto>> =
     this.foldMap(interpreter, MF)
-
-/**
- * Module definition (Data Source methods). Here we lift to the Free context all the operation blocks defined on the algebra.
- */
-fun getAllHeroes(): FreeHeroesAlgebra<List<CharacterDto>> =
-    Free.liftF(HeroesAlgebra.GetAll)
-
-fun getSingleHero(heroId: String): FreeHeroesAlgebra<CharacterDto> =
-    Free.liftF(HeroesAlgebra.GetSingle(heroId))
-
-fun handlePresentationEffects(result: Either<CharacterError, List<CharacterDto>>): FreeHeroesAlgebra<Unit> =
-    Free.liftF(HeroesAlgebra.HandlePresentationEffects(result))
-
-fun <A> attempt(fa: FreeHeroesAlgebra<A>): FreeHeroesAlgebra<Either<Throwable, A>> =
-    Free.liftF(HeroesAlgebra.Attempt(fa))
-
