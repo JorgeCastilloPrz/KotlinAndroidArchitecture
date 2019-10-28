7a9b56f6358e169d401d2f4316192a090c1fe44a
Jorge Castillo Pérez <jorge.castillo.prz@gmail.com>
2019-10-28 11:03:22 +0100

    Some minor code polishments

--- app/src/main/java/com/github/jorgecastillo/kotlinandroid/io/runtime/ui/SuperHeroDetailActivity.kt
+++ app/src/main/java/com/github/jorgecastillo/kotlinandroid/io/runtime/ui/SuperHeroDetailActivity.kt
@@ -18,87 +18,83 @@ import com.github.jorgecastillo.kotlinandroid.io.algebras.ui.model.HeroViewState
 import com.github.jorgecastillo.kotlinandroid.io.runtime.context.RuntimeContext
 import com.github.jorgecastillo.kotlinandroid.io.runtime.context.runtime
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_detail.appBar
-import kotlinx.android.synthetic.main.activity_detail.collapsingToolbar
-import kotlinx.android.synthetic.main.activity_detail.description
-import kotlinx.android.synthetic.main.activity_detail.headerImage
-import kotlinx.android.synthetic.main.activity_detail.loader
+import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.coroutines.Dispatchers
 
 class SuperHeroDetailActivity : AppCompatActivity(), SuperHeroDetailView {
 
-  companion object {
-    const val EXTRA_HERO_ID = "EXTRA_HERO_ID"
+    companion object {
+        const val EXTRA_HERO_ID = "EXTRA_HERO_ID"
 
-    fun launch(
-      source: Context,
-      heroId: String
-    ) {
-      val intent = Intent(source, SuperHeroDetailActivity::class.java)
-      intent.putExtra(EXTRA_HERO_ID, heroId)
-      source.startActivity(intent)
-    }
-  }
-
-  private val ctx = RuntimeContext(
-    bgDispatcher = Dispatchers.IO,
-    mainDispatcher = Dispatchers.Main
-  )
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_detail)
-  }
-
-  override fun onResume() {
-    super.onResume()
-    intent.extras?.let {
-      val heroId = it.getString(EXTRA_HERO_ID)
-      if (heroId == null) {
-        closeWithError()
-      } else {
-        unsafe {
-          runNonBlocking({
-            IO.runtime(ctx)
-              .getSuperHeroDetails(heroId, this@SuperHeroDetailActivity)
-          }, {})
+        fun launch(
+                source: Context,
+                heroId: String
+        ) {
+            val intent = Intent(source, SuperHeroDetailActivity::class.java)
+            intent.putExtra(EXTRA_HERO_ID, heroId)
+            source.startActivity(intent)
         }
-      }
-    } ?: closeWithError()
-  }
+    }
 
-  private fun closeWithError() {
-    Toast.makeText(this, string.hero_id_needed, Toast.LENGTH_SHORT)
-      .show()
-  }
+    private val ctx = RuntimeContext(
+            bgDispatcher = Dispatchers.IO,
+            mainDispatcher = Dispatchers.Main
+    )
 
-  override fun showLoading() {
-    loader.visibility = View.VISIBLE
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
 
-  override fun hideLoading() {
-    loader.visibility = View.GONE
-  }
+    override fun onResume() {
+        super.onResume()
+        intent.extras?.let {
+            val heroId = it.getString(EXTRA_HERO_ID)
+            if (heroId == null) {
+                closeWithError()
+            } else {
+                loadHeroDetails(heroId)
+            }
+        } ?: closeWithError()
+    }
 
-  override fun drawHero(hero: HeroViewState) {
-    collapsingToolbar.title = hero.name
-    description.text =
-      hero.description.let { if (it.isNotEmpty()) it else getString(string.empty_description) }
-    headerImage.loadImageAsync(hero.photoUrl)
-  }
+    private fun loadHeroDetails(heroId: String) {
+        unsafe {
+            runNonBlocking({
+                IO.runtime(ctx).getSuperHeroDetails(heroId, this@SuperHeroDetailActivity)
+            }, {})
+        }
+    }
 
-  override fun showNotFoundError() {
-    Snackbar.make(appBar, string.not_found, Snackbar.LENGTH_SHORT)
-      .show()
-  }
+    private fun closeWithError() {
+        Toast.makeText(this, string.hero_id_needed, Toast.LENGTH_SHORT).show()
+    }
 
-  override fun showGenericError() {
-    Snackbar.make(appBar, string.generic, Snackbar.LENGTH_SHORT)
-      .show()
-  }
+    override fun showLoading() {
+        loader.visibility = View.VISIBLE
+    }
 
-  override fun showAuthenticationError() {
-    Snackbar.make(appBar, string.authentication, Snackbar.LENGTH_SHORT)
-      .show()
-  }
+    override fun hideLoading() {
+        loader.visibility = View.GONE
+    }
+
+    override fun drawHero(hero: HeroViewState) {
+        collapsingToolbar.title = hero.name
+        description.text = hero.description.let {
+            if (it.isNotEmpty()) it else getString(string.empty_description)
+        }
+        headerImage.loadImageAsync(hero.photoUrl)
+    }
+
+    override fun showNotFoundError() {
+        Snackbar.make(appBar, string.not_found, Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun showGenericError() {
+        Snackbar.make(appBar, string.generic, Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun showAuthenticationError() {
+        Snackbar.make(appBar, string.authentication, Snackbar.LENGTH_SHORT).show()
+    }
 }
